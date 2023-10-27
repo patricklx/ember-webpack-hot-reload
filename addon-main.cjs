d1d5878e18b7ba5b4d7af75ad58f367bccc43508
patrick <pa.trickjlp@gmail.com>
2023-10-27 09:33:07 +0200

    load babel plugin

--- addon-main.cjs
+++ addon-main.cjs
@@ -2,3 +2,21 @@
 
 const { addonV1Shim } = require('@embroider/addon-shim');
 module.exports = addonV1Shim(__dirname);
+const included = module.exports.included;
+
+Object.assign(module.exports, {
+  _getBabelOptions() {
+    const parentOptions = this.parent && this.parent.options;
+    const appOptions = this.app && this.app.options;
+    const addonOptions = parentOptions || appOptions || {};
+
+    addonOptions.babel = addonOptions.babel || {};
+    addonOptions.babel.plugins = addonOptions.babel.plugins || [];
+    return addonOptions.babel;
+  },
+  included(...args) {
+    this._getBabelOptions().plugins.splice(0, 0, [require.resolve('./dist/babel-plugin')]);
+    included.call(this, ...args);
+  }
+})
+
