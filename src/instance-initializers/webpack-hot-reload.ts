fa0a1de98dfd0547acbaf4b183d1e94ffe4821b5
Patrick Pircher <patricklx@users.noreply.github.com>
2023-11-09 11:56:01 +0100

    fix resolver

--- src/instance-initializers/webpack-hot-reload.ts
+++ src/instance-initializers/webpack-hot-reload.ts
@@ -1,8 +1,8 @@
 export function initialize(applicationInstance) {
   applicationInstance.lookup('service:webpack-hot-reload');
-  const Resolver = applicationInstance.application.Resolver;
-  const resolverResolve = Resolver.prototype.resolve;
-  Resolver.prototype.resolve = function(name: string) {
+  const resolver = application.__registry__.resolver._fallback || application.__registry__.resolver;
+  const resolverResolve = resolver.resolve;
+  resolver.resolve = function (name) {
     name = name.replace(/--hot-version--.*$/, '');
     return resolverResolve.call(this, name);
   };
